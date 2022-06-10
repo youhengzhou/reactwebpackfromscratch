20d1c0c700153dfb14545df8ff3476319d8e6030
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-06-09 23:03:53 -0400

    refactor

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let word = "applebee        ";
 function frontDoorPassword(word) {
   let a = word.slice(0, 1);
   a = a.toUpperCase();
-  console.log(a);
   return word[0].toUpperCase() + word.slice(1, word.length).toLowerCase();
 }
 

4b3ec31475907eb78c41293b4860a9289ba908d2
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-06-09 23:03:27 -0400

    add exercise index

---
+++ index.js
@@ -0,0 +1,12 @@
+let word = "applebee        ";
+
+function frontDoorPassword(word) {
+  let a = word.slice(0, 1);
+  a = a.toUpperCase();
+  console.log(a);
+  return word[0].toUpperCase() + word.slice(1, word.length).toLowerCase();
+}
+
+output = frontDoorPassword(word);
+
+console.log(output);

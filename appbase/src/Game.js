f570217277d820e21e9d5a7af8cf48c75ac8e61d
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-06-11 23:34:58 -0400

    some changes

--- appbase/src/Game.js
+++ appbase/src/Game.js
@@ -2,14 +2,36 @@ import React from "react";
 import "./MainCSS/game.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
